002196373fe98210c83238cf8df2d5715dbfe230
Zakkatx1 <elmira03122007@mail.ru>
2025-12-04 21:17:55 +0500

    привязала гитхаб и заполнила данные всех файлов

--- someapp/urls.py
+++ someapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import home, product_list, product_detail
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home, name='home'),  # главная страница по адресу /
     path('products/', product_list, name='product_list'),
     path('products/<int:pk>/', product_detail, name='product_detail'),
-    path('category/<slug:slug>/', category_products, name='category_products'),
 ]
--- someproject/urls.py
+++ someproject/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('someapp.urls'))
+    path('app/', include('someapp.urls')),  # ваше приложение
+    path('', include('someapp.urls')),  # добавьте эту строку для главной страницы
 ]
 
 if settings.DEBUG:
